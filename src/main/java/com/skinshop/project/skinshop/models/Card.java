736446e479be6acb60b5072a4c49283c78e404b1
mayumeli <mayu.satori@mercadolivre.com>
2022-11-02 15:37:48 -0300

    remove dead code

--- src/main/java/com/skinshop/project/skinshop/models/Card.java
+++ src/main/java/com/skinshop/project/skinshop/models/Card.java
@@ -1,6 +1,5 @@
 package com.skinshop.project.skinshop.models;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- src/main/java/com/skinshop/project/skinshop/models/Sales.java
+++ src/main/java/com/skinshop/project/skinshop/models/Sales.java
@@ -1,9 +1,7 @@
 package com.skinshop.project.skinshop.models;
 
-import java.util.Calendar;
 import java.util.Date;
 
-import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,7 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
